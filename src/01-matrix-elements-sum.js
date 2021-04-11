ec05d6ca556a6f516e9908b892ced363d4d07418
Anastasia Marhun <anastasiamarhun@gmail.com>
2021-04-11 15:25:54 +0300

    feat: add solution to matrix-elements-sum

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 /**
  * Given matrix, a rectangular matrix of integers,
  * just add up all the values that don't appear below a "0".
@@ -14,8 +15,18 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  const [row] = matrix;
+  const revertedMatrix = row.map((value, column) => matrix.map((row1) => row1[column]));
+
+  let sum = 0;
+  revertedMatrix.forEach((elem) => {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i] === 0 ? i = elem.length : sum += elem[i];
+    }
+  });
+
+  return sum;
 }
 
 module.exports = getMatrixElementsSum;
